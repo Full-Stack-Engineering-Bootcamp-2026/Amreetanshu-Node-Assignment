dc081c8e2e464c3661f818ce2a3e3c33be6c5b95
Amreetanshu Sinha <amreetanshu.sinha@mindbowser.com>
2026-04-06 01:04:34 +0530

    feat-day13 sequalize

---
+++ util/database.js
@@ -0,0 +1,24 @@
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'Node-Starter',
+//     password: 'Root@1234'
+// });
+
+// module.exports = pool.promise();
+
+// without using the orm we used to do that upper code 
+//now with sequelize we will do 
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('Node-Starter','root','Root@1234',{
+    dialect:'mysql',
+    host:'localhost'
+});
+//we need to mention the dbname , user and the password  and a object as a 
+// 4th param which is not mandatory 
+
+module.exports = sequelize;
