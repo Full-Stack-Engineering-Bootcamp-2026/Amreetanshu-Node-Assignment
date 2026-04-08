05e21780992dfac2ce4dbf459c191f47af3e11ff
Amreetanshu Sinha <amreetanshu.sinha@mindbowser.com>
2026-04-08 18:13:32 +0530

    feat-day16,mongoDb-BasicCrud

--- models/product.js
+++ models/product.js
@@ -3,24 +3,75 @@
 // const sequelize = require('../util/database');
 
 // to use it for the mongoDb connection
+const mongoDb = require('mongodb');
+const { getDb } = require('../util/database');
 const getdb = require("../util/database").getDb;
 class Product {
-  constructor(title,price,description,imageUrl){
+  constructor(title,price,description,imageUrl, id){
     this.title=title;
     this.price=price;
     this.description=description;
     this.imageUrl=imageUrl;
+    this._id = id ? new mongoDb.ObjectId(id):null;
   }
 
   save(){
-    const db = getdb();
-    db.collection('products').insertOne(this).then(
+    const db = getdb(); 
+    let dbOp;
+    if(this._id){
+      //update the product 
+      dbOp = db.collection('products').updateOne({_id: mongoDb.ObjectId(this._id)});
+    }
+    else{
+      dbOp = db
+      .collection('products')
+      .insertOne(this)
+    }
+   return dbOp.then(
       result => {
         console.log(result);
       }
     ).catch( err =>{
       console.log(err);
     })   // here we can pass the object 
+  }
+
+  static fetchAll(){
+    const db = getdb();  // always we are calling this method 
+    return db.collection('products').find().toArray().then(
+      product =>{
+        console.log(product);
+        return product;
+      }
+    ).catch(
+      err =>{
+        console.log(err)
+      }
+    );  // to call all the products 
+  }
+
+  static findById(prodId){
+    const db = getdb();
+    return db.collection('products').find({_id : new mongoDb.ObjectId(prodId)}).next().then(
+      (product)=>{
+        console.log(product);
+        return product;
+      }
+    ).catch(
+      (err)=>{console.log(err)}
+    );
+  }
+
+  static deleteByid(prodId){
+    const db = getDb();
+    return db.collection('products').deleteOne({_id: new mongoDb.ObjectId(prodId)})
+    .then(
+      (result)=>{console.log(result);}
+    ).catch(
+      (err)=>{
+        console.log(err);
+      }
+    )
   }
 }
 
