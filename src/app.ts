551dc163233f6373b1dc76f6be34aaf5912a4aa2
Amreetanshu Sinha <amreetanshu.sinha@mindbowser.com>
2026-04-27 12:41:47 +0530

    feat-day26 corsMiddleware

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,9 @@ import emailRoutes from "./routes/email.routes";
 import fileRoutes from "./routes/file.routes";
 import UserPageRoutes from "./routes/UserPage.routes"
 import { connectDB } from "./utils/Database";
+import postRoute from "./routes/PostRoute";
+import { corsMiddleware } from "./middleware/cors.middleware";
+
 
 connectDB();
 
@@ -13,16 +16,29 @@ connectDB();
 const app = express();
 
 
-app.use(express.json());
+// app.use(express.json());
+// //app.use(cors());
+
+// // this will allow == all the user as ip is not specified 
+
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173"
+//   })
+// );  // we are allowing this specific origin 
+
+
+
 
 
 console.log("SENDGRID KEY:", process.env.SENDGRID_API_KEY);
 console.log("SENDER EMAIL:", process.env.SENDER_EMAIL);
 
-
+app.use(corsMiddleware);
 app.use("/", emailRoutes);
 app.use("/",fileRoutes);
 app.use("/",UserPageRoutes)
+app.use("/v1",postRoute)
 
 
 app.get("/", (req, res) => {
